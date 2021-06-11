7d69eb0f818e619f619f6846d3bae43a5447571a
sabbirshiam <sabbirshiam@gmail.com>
2021-06-11 11:30:23 -0600

    find Single Number.

---
+++ src/main/kotlin/SingleNumber.kt
@@ -0,0 +1,35 @@
+/*
+* Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+* You must implement a solution with a linear runtime complexity and use only constant extra space.
+* Example 1
+* Input: nums = [2,2,1]
+* Output: 1
+*
+* Example 2:
+* Input: nums = [4,1,2,1,2]
+* Output: 4
+ */
+
+fun main(args: Array<String>) {
+    val array1 = intArrayOf(1, 1, 2)
+    val arrLength = SingleNumber().singleNumber(array1)
+    println(arrLength)
+}
+
+class SingleNumber {
+    fun singleNumber(nums: IntArray): Int {
+        val tempContainer = HashMap<Int, Int>()
+
+        for (num in nums) {
+
+            if (tempContainer.containsKey(num)) {
+                tempContainer[num] = tempContainer[num]?.plus(1) ?: 0
+            } else {
+                tempContainer[num] = 0
+            }
+        }
+
+        return tempContainer.filterValues { it == 0 }.keys.first()
+    }
+}
